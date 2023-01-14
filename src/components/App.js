46fc238edc225936f76ce84293769ca180897165
Md Rizwan Ansari <85256728+mdrizwan7739@users.noreply.github.com>
2023-01-14 21:05:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-const App = () => {
+function App() {
+  let [state, setState] = useState(0);
+  let [num, newnum] = useState(0);
   return (
-    <></>
-  )
-}
+    <>
 
+      <button data-testid="button-prev" disabled={num === 0 ? true : false} onClick={() => { setState(state - 1); newnum(num - 1) }}>Prev</button>
+      <button data-testid="button-next" disabled={num === 4 ? true : false} onClick={() => { setState(state + 1); newnum(num + 1) }}>Next</button>
+      <button data-testid="button-restart" disabled={num === 0 ? true : false} onClick={() => { setState(0); newnum(0) }}>Restart</button>
+      <h1 data-testid="title">{slides[state].text}</h1>
+      <p data-testid="text">{slides[state].text}</p>
+    </>
+  );
+
+
+}
 
 export default App;
